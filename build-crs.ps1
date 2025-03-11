4ef179c747feae6c0b5a75c2416b5c0fdf8e42b4
Ilya Kochetkov <ie.kochetkov@yandex.ru>
2025-03-11 12:16:14 +0300

    Упрощение скриптов сборки Docker: вынесение чтения переменных окружения и проверки логина в отдельные функции, оптимизация команд сборки образов.

--- build-crs.ps1
+++ build-crs.ps1
@@ -1,57 +1,12 @@
-$envFilePath = ".env"
+Import-Module "./HelperBuild.psm1"
 
-# Чтение .env файла и загрузка переменных окружения
-if (Test-Path $envFilePath) {
-    Get-Content $envFilePath | ForEach-Object {
-        if ($_ -match '^(?<key>[^=]+)=(?<value>.*)$') {
-            $key = $matches['key'].Trim()
-            $value = $matches['value'].Trim()
-            [System.Environment]::SetEnvironmentVariable($key, $value)
-        }
-    }
-}
-
-if (-not [string]::IsNullOrEmpty($env:DOCKER_LOGIN) -and -not [string]::IsNullOrEmpty($env:DOCKER_PASSWORD) -and -not [string]::IsNullOrEmpty($env:DOCKER_REGISTRY_URL)) {
-    docker login -u $env:DOCKER_LOGIN -p $env:DOCKER_PASSWORD $env:DOCKER_REGISTRY_URL
-    if ($LASTEXITCODE -ne 0) {
-        Write-Host "Docker login failed"
-        exit 1
-    }
-} else {
-    Write-Host "Skipping Docker login due to missing credentials"
-}
-
-if ($env:DOCKER_SYSTEM_PRUNE -eq 'true') {
-    docker system prune -af
-}
+Read-EnvFile
+Test-DockerRegistryLogin
 
 docker build `
-    --pull `
-    --build-arg DOCKER_REGISTRY_URL=library `
-    --build-arg BASE_IMAGE=ubuntu `
-    --build-arg BASE_TAG=20.04 `
-    --build-arg ONESCRIPT_PACKAGES="yard" `
-    -t "$($env:DOCKER_REGISTRY_URL)/oscript-downloader:latest" `
-    -f oscript/Dockerfile .
-
-docker build `
-    --build-arg ONEC_USERNAME=$env:ONEC_USERNAME `
-    --build-arg ONEC_PASSWORD=$env:ONEC_PASSWORD `
     --build-arg ONEC_VERSION=$env:ONEC_VERSION `
     --build-arg DOCKER_REGISTRY_URL=$env:DOCKER_REGISTRY_URL `
-    --build-arg BASE_IMAGE=oscript-downloader `
-    --build-arg BASE_TAG=latest `
-    -t "$($env:DOCKER_REGISTRY_URL)/onec-server:$($env:ONEC_VERSION)" `
-    -f server/Dockerfile .
-
-docker build `
-    --build-arg ONEC_USERNAME=$env:ONEC_USERNAME `
-    --build-arg ONEC_PASSWORD=$env:ONEC_PASSWORD `
-    --build-arg ONEC_VERSION=$env:ONEC_VERSION `
-    --build-arg DOCKER_REGISTRY_URL=$env:DOCKER_REGISTRY_URL `
-    --build-arg BASE_IMAGE=onec-server `
-    --build-arg BASE_TAG=$env:ONEC_VERSION `
-    -t "$($env:DOCKER_REGISTRY_URL)/crs:$($env:ONEC_VERSION)" `
-    -f crs/Dockerfile .
+    --tag "$($env:DOCKER_REGISTRY_URL)/crs:$($env:ONEC_VERSION)" `
+    --file crs/Dockerfile .
 
 docker push "$($env:DOCKER_REGISTRY_URL)/crs:$($env:ONEC_VERSION)"
--- build-pusk.ps1
+++ build-pusk.ps1
@@ -1,25 +1,7 @@
-$envFilePath = ".env"
+Import-Module "./HelperBuild.psm1"
 
-# Чтение .env файла и загрузка переменных окружения
-if (Test-Path $envFilePath) {
-    Get-Content $envFilePath | ForEach-Object {
-        if ($_ -match '^(?<key>[^=]+)=(?<value>.*)$') {
-            $key = $matches['key'].Trim()
-            $value = $matches['value'].Trim()
-            [System.Environment]::SetEnvironmentVariable($key, $value)
-        }
-    }
-}
-
-if (-not [string]::IsNullOrEmpty($env:DOCKER_LOGIN) -and -not [string]::IsNullOrEmpty($env:DOCKER_PASSWORD) -and -not [string]::IsNullOrEmpty($env:DOCKER_REGISTRY_URL)) {
-    docker login -u $env:DOCKER_LOGIN -p $env:DOCKER_PASSWORD $env:DOCKER_REGISTRY_URL
-    if ($LASTEXITCODE -ne 0) {
-        Write-Host "Docker login failed"
-        exit 1
-    }
-} else {
-    Write-Host "Skipping Docker login due to missing credentials"
-}
+Read-EnvFile
+Test-DockerRegistryLogin
 
 docker build `
     --tag "$($env:DOCKER_REGISTRY_URL)/pusk:1.2.1" `
