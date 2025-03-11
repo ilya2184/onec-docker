3856724e417a5441d1f33f6ccd6d2d9189316101
Ilya Kochetkov <ie.kochetkov@yandex.ru>
2025-03-11 20:39:03 +0300

    Update build-edt.ps1

--- build-edt.ps1
+++ build-edt.ps1
@@ -8,10 +8,5 @@ Start-DistribWebServer -distrPath $distrPath -distrWebPort 8088
 $distrHost = "host.docker.internal:8088"
 
 Build-DockerImage -buildType "edt" -distrHost $distrHost
-Build-DockerImage -buildType "onec-server" -distrHost $distrHost
-Build-DockerImage -buildType "crs" -distrHost $distrHost
-Build-DockerImage -buildType "crs-apache" -distrHost $distrHost
-Build-DockerImage -buildType "onec-client" -distrHost $distrHost
-Build-DockerImage -buildType "onec-client-vnc" -distrHost $distrHost
 
 Stop-DistribWebServer
